f7cd8dc84fc9a946210bd5f68c0c381500f4bc62
manzhaimaksim <maxbbb88@gmail.com>
2020-04-19 01:26:05 +0300

    Editing class Post

--- post.rb
+++ post.rb
@@ -1,10 +1,48 @@
+require 'sqlite3'
+
 class Post
+  @@SQLITE_DB_FILE = 'notepad.sqlite'
+
   def self.post_types
-    [Memo, Link, Task]
+    { Memo: Memo, Task: Task, Link: Link }
   end
 
   def self.create(type_index)
-    return post_types[type_index].new
+    post_types[type_index].new
+  end
+
+  def self.find(limit, type, id)
+    db = SQLite3::Database.open(@@SQLITE_DB_FILE)
+
+    if id.present?
+      db.results_as_hash = true
+      result = result[0] if result.is_a? Array
+      db.close
+
+      if result.empty?
+        puts 'Такой id #{id} не найден в базе'
+        return nil
+      else
+        post = create(result['type'])
+        post.load_data(result)
+        return post
+      end
+    else
+      db.results_as_hash = false
+      query = 'SELECT rowid, * FROM posts '
+      query += 'WHERE type = :type ' unless type.nil?
+      query += 'ORDER by rowid DESC'
+      query += 'LIMIT :limit ' unless limit.nil?
+
+      statement = db.prepare query
+      statement.bind_param('type', type) unless type.nil?
+      statement.bind_param('limit', limit) unless limit.nil?
+
+      result = statement.execute!
+      statement.close
+      db.close
+      return result
+    end
   end
 
   def initialize
@@ -18,8 +56,31 @@ class Post
   def to_strings
   end
 
+  def to_db_hash
+    {
+        'type' => self.class.name,
+        'created_at' => @created_at.to_s
+    }
+  end
+
+  def save_to_db
+    db = SQLite3::Database.open(@@SQLITE_DB_FILE)
+    db.results_as_hash = true
+
+    db.execute(
+        'INSERT INTO posts (' + to_db_hash.keys.join(', ') + ' VALUES ( ' + ('?,'*to_db_hash.size).chomp(', ') + ')', to_db_hash.values
+    )
+    insert_row_id = db.last_insert_row_id
+    db.close
+    return insert_row_id
+  end
+
+  def load_data(data_hash)
+    @created_at = Time.parse(data_hash['created_at'])
+  end
+
   def save
-    file = File.new(file_path, "w:UTF-8")
+    file = File.new(file_path, 'w:UTF-8')
 
     for item in to_strings do
       file.puts(item)
@@ -31,6 +92,6 @@ class Post
   def file_path
     current_path = File.dirname(__FILE__)
     file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
-    return current_path + "/" + file_name
+    current_path + '/' + file_name
   end
 end
